86e477900d9ebd3eaa2ab5c55d9500b0d3c63595
Yogendra Singh <yogendra@clever.coffee>
2019-07-24 18:28:16 +0530

    Setup React state of component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import Note from './Note/Note'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {id: 1, noteContent: "Note 1 here!"},
+                {id: 2, noteContent: "Note 2 here!"},
+
+            ],
+        }
+    }
+
+    render() {
+        return (
+            <div className="notesWrapper">
+                <div className="notesHeader">
+                    <div className="heading">React ToDo List using firebase</div>
+                </div>
+                <div className="notesBody">
+                    {
+                        this.state.notes.map((note) => {
+                            return (
+                                <Note noteContent={note.noteContent} noteId={note.id} key={note.id}/>
+
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
+        );
+    }
+
 }
+
 
 export default App;
